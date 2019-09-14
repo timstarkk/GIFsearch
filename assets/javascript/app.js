57a22900ce6c29da1156924068c2531e099ef649
timstarkk <timstarkk@icloud.com>
2019-09-14 01:36:12 -0500

    changed search result amount to ten instead of 8

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -20,21 +20,24 @@ function createSearchButton(term) {
 
 
 function searchTerm(search) {
-    const queryURL = "https://api.giphy.com/v1/gifs/search?q=" + search + "&api_key=CqZy82L3gwNLs4EHQAOzV3mswCe9WE6U&limit=8";
+    const queryURL = "https://api.giphy.com/v1/gifs/search?q=" + search + "&api_key=CqZy82L3gwNLs4EHQAOzV3mswCe9WE6U&limit=10";
     $.ajax({
         url: queryURL,
         method: "GET",
     }).then(function (response) {
         data = response.data;
+        console.log(data);
 
         $('#one').html(`<img class="still" src="${data[0].images.original_still.url}" height="${data[0].images.original.height} width="${data[0].images.original.width}>`)
         $('#two').html(`<img class="still" src="${data[1].images.original_still.url}" height="${data[1].images.original.height} width="${data[1].images.original.width}>`)
         $('#three').html(`<img class="still" src="${data[2].images.original_still.url}" height="${data[2].images.original.height} width="${data[2].images.original.width}>`)
         $('#four').html(`<img class="still" src="${data[3].images.original_still.url}" height="${data[3].images.original.height} width="${data[3].images.original.width}>`)
         $('#five').html(`<img class="still" src="${data[4].images.original_still.url}" height="${data[4].images.original.height} width="${data[4].images.original.width}>`)
-        $('#six').html(`<img class="still" src="${data[5].images.original_still.url}" height="${data[4].images.original.height} width="${data[4].images.original.width}>`)
-        $('#seven').html(`<img class="still" src="${data[6].images.original_still.url}" height="${data[4].images.original.height} width="${data[4].images.original.width}>`)
-        $('#eight').html(`<img class="still" src="${data[7].images.original_still.url}" height="${data[4].images.original.height} width="${data[4].images.original.width}>`)
+        $('#six').html(`<img class="still" src="${data[5].images.original_still.url}" height="${data[5].images.original.height} width="${data[5].images.original.width}>`)
+        $('#seven').html(`<img class="still" src="${data[6].images.original_still.url}" height="${data[6].images.original.height} width="${data[6].images.original.width}>`)
+        $('#eight').html(`<img class="still" src="${data[7].images.original_still.url}" height="${data[7].images.original.height} width="${data[7].images.original.width}>`)
+        $('#nine').html(`<img class="still" src="${data[8].images.original_still.url}" height="${data[8].images.original.height} width="${data[8].images.original.width}>`)
+        $('#ten').html(`<img class="still" src="${data[9].images.original_still.url}" height="${data[9].images.original.height} width="${data[9].images.original.width}>`)
     });
 }
 
